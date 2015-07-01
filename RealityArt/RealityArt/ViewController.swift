b34852f9e4264f59baca87cf9e205d923754ff0d
Chun Chen <cchen1@wl00466300.active.local>
2015-07-01 15:52:28 +0800

    initial

---
+++ RealityArt/RealityArt/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  RealityArt
+//
+//  Created by Chun Chen on 6/30/15.
+//  Copyright (c) 2015 Chun Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        println("default")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
