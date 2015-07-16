d6733879d7447b7e673afb8688b6b7ec185c57f1
Chun Chen <cchen1@wl00466296.active.local>
2015-07-16 17:13:35 +0800

    tuning

---
+++ RealityArt/RealityArt/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  RealityArt
+//
+//  Created by Chun Chen on 7/16/15.
+//  Copyright (c) 2015 Chun Chen. All rights reserved.
+//
+
+public struct Constants {
+    
+    static let UI_MAIN_TABLE_CELL_RATIO : Float = 9.0 / 16.0
+    
+}
--- RealityArt/RealityArt/MainTableViewController.swift
+++ RealityArt/RealityArt/MainTableViewController.swift
@@ -34,7 +34,7 @@ class MainTableViewController: UITableViewController, UITableViewDelegate, UITab
     }
     
     override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return CGFloat(self.view.frame.size.width * 9 / 16)
+        return CGFloat(Float(self.view.frame.size.width) * Constants.UI_MAIN_TABLE_CELL_RATIO)
     }
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
